dde532fbdac67818e2a2f0232e8619618145e7ed
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-03 22:02:04 +0530

    Create To find sum of arithematic progression.py

---
+++ To find sum of arithematic progression.py
@@ -0,0 +1,13 @@
+'''To find sum of arithematic progression '''
+n,a,d=input().split()
+n=int(n)
+a=int(a)
+d=int(d)
+sum= (n*(2*a+(n-1)*d))/2
+print(int(sum)) '''
+n,a,d=input().split()
+n=int(n)
+a=int(a)
+d=int(d)
+sum= (n*(2*a+(n-1)*d))/2
+print(int(sum))
