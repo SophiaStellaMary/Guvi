5c2bb26fc506ae204033afc288274c55d1f8c872
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-03 21:39:20 +0530

    Update and rename PROGRAM TO FIND SUM OF ARITHMETIC PROGRESSION .py to To find sum of arithemtic progression.py

--- PROGRAM TO FIND SUM OF ARITHMETIC PROGRESSION .py
+++
@@ -1,7 +0,0 @@
-''' PROGRAM TO FIND SUM OF ARITHMETIC PROGRESSION '''
-n,a,d=input().split()
-n=int(n)
-a=int(a)
-d=int(d)
-total = (n*(2*a+(n-1)*d))/2
-print(int(total))
---
+++ To find sum of arithemtic progression.py
@@ -0,0 +1,7 @@
+''' To find sum of arithemtic progression'''
+n,a,d=input().split()
+n=int(n)
+a=int(a)
+d=int(d)
+sum= (n*(2*a+(n-1)*d))/2
+print(int(sum))
