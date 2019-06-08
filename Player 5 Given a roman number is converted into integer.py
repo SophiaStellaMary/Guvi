f5ff457add77f986056911b6a98a4ca37753e55c
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-08 06:28:15 +0530

    Create Player 5 Given a roman number is converted into integer.py

---
+++ Player 5 Given a roman number is converted into integer.py
@@ -0,0 +1,4 @@
+'''Player 5 Given a roman number is converted into integer'''
+i=input()
+roman=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+print(roman.index(i))
