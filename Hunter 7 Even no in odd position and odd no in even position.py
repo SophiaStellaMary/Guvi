b65cea4480abadb7aeccffaa11c0f9370a0c69c6
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-10 12:02:10 +0530

    Update Hunter 7 Even no in odd position and odd no in even position.py

--- Hunter 7 Even no in odd position and odd no in even position.py
+++ Hunter 7 Even no in odd position and odd no in even position.py
@@ -1,10 +1,10 @@
 '''Hunter 7 Even no in odd position and odd no in even position'''
-a=int(input())
-b=list(map(int,input().split()))
-for i in range(0,a):
+x=int(input())
+y=[int(x) for x in input().split()]
+for i in range(0,x):
     if(i%2==0):
-        if(b[i]%2!=0):
-            print(b[i],end=" ")
+        if(y[i]%2!=0):
+            print(y[i],end=" ")
     elif(i%2!=0):
-        if(b[i]%2==0):
-            print(b[i],end=" ")
+        if(y[i]%2==0):
+            print(y[i],end=" ")
