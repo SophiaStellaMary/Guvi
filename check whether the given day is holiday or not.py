bafdb75b4f084d3430b9619dda92ca7ab8bc8e94
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-11 15:18:02 +0530

    Create check whether the given day is holiday or not.py

---
+++ check whether the given day is holiday or not.py
@@ -0,0 +1,6 @@
+'''Player 11 check whether the given day is holiday or not'''
+n=input() 
+if(n=='Saturday' or n=='Sunday'):
+  print('yes')
+elif(n=='Monday'or n=='Tuesday'or n=='Wednesday'or n=='Thursday'or n=='Friday'):
+  print('no')
