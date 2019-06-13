86a8d8870bf0228e17b148ea05901cc8b1cf7ee3
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-13 09:12:21 +0530

    Create Program to print the element that appeared in all array.py

---
+++ Program to print the element that appeared in all array.py
@@ -0,0 +1,8 @@
+'''Hunter 18 print the element that appeared in all array'''
+X,Y=map(int,input().split())
+L=[]
+for i in range(X):
+	s=set(map(int,input().split()))
+	L.append(s)
+c=s.intersection(*L)
+print(*c)
