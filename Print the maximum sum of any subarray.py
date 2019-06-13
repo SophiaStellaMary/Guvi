0e47a92063c74852c72315963c384add5f13b44f
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-13 12:46:07 +0530

    Create Print the maximum sum of any subarray.py

---
+++ Print the maximum sum of any subarray.py
@@ -0,0 +1,9 @@
+'''Hunter 29 Print the maximum sum of any subarray'''
+n1=int(input())
+l=list(map(int,input().split()))
+p=[]
+for i in range(n1-1):
+	for j in range(i,n1):
+		c=l[i:j+1]
+		p.append(sum(c))
+print(max(p))
