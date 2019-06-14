ed1cf0d1888f1c599169170dd6023a1682bd7c7e
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-14 12:19:23 +0530

    Create Program to count thre perfect square in a given interval.py

---
+++ Program to count thre perfect square in a given interval.py
@@ -0,0 +1,14 @@
+'''PLAYER 29 count thre perfect square in a given interval '''
+a,b=map(int,input().split())
+l1=[]
+c=0
+for i in range(1,b+1):
+    sq=i*i
+    if(sq<=b):
+        l1.append(i*i)
+    else:
+        break
+for j in range(a,b+1):
+    if(j in l1):
+        c+=1
+print(c)    
