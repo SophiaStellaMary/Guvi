c725724f5f4bb004272bab72f77d0acd8509e90d
SophiaStellaMary <50897806+SophiaStellaMary@users.noreply.github.com>
2019-06-15 12:02:11 +0530

    Create Program to find  the given number is present in the list.py

---
+++ Program to find  the given number is present in the list.py
@@ -0,0 +1,7 @@
+'''Player 32 program to find  the given number is present in the list'''
+x,y=list(map(int,input().split()))
+l=list(map(int,input().split()))
+if y in l:
+  print("Yes")
+else:
+  print("No")
